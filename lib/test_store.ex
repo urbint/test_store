db350911e01de41c2bc1a2bce3dcd9903919c48e
Ryan Schmukler <rschmukler@gmail.com>
2017-01-16 18:12:24 -0500

    initial commit

---
+++ lib/test_store.ex
@@ -0,0 +1,18 @@
+defmodule TestStore do
+  @moduledoc """
+  Documentation for TestStore.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestStore.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/test_store_test.exs
@@ -0,0 +1,8 @@
+defmodule TestStoreTest do
+  use ExUnit.Case
+  doctest TestStore
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
